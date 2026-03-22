19b3c656047e94aa98d00a8d9d1365ae0b577077
Architector <katusha2105@gmail.com>
2026-03-22 18:38:04 +0200

    Fix flake8: line length and newline at EOF

--- Desktop/py-mutable-immutable/py-mutable-immutable/app/create_dictionary.py
+++ Desktop/py-mutable-immutable/py-mutable-immutable/app/create_dictionary.py
@@ -3,11 +3,14 @@ from typing import Any, Dict
 def create_dictionary(*args: Any) -> Dict[Any, int]:
     result: Dict[Any, int] = {}
     for index, arg in enumerate(args):
-        # дозволені типи для ключів словника
-        if isinstance(arg, (int, float, str, bool, type(None))) or callable(arg):
+        if (
+    isinstance(arg, (int, float, str, bool, type(None), tuple))
+    or callable(arg)
+):
             result[arg] = index
         else:
             print(f"Cannot add {arg} to the dict!")
     return result
+
 
 
