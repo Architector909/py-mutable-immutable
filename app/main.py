a084e6c22d7806e6858d0231a666e6e4349d90db
Architector <katusha2105@gmail.com>
2026-03-22 15:19:07 +0200

    Add sorted_variables and required variables

--- app/main.py
+++ app/main.py
@@ -1,19 +1,23 @@
-lucky_number = 777
-pi = 3.14
-one_is_a_prime_number = False
-name = "Richard"
-my_favourite_films = [
-    "The Shawshank Redemption",
-    "The Lord of the Rings: The Return of the King",
-    "Pulp Fiction",
-    "The Good, the Bad and the Ugly",
-    "The Matrix",
-]
-profile_info = ("michel", "michel@gmail.com", "12345678")
-marks = {
-    "John": 4,
-    "Sergio": 3,
-}
-collection_of_coins = {1, 2, 25}
+# app/main.py
 
-# write your code here
+# Задаємо змінні для тестів
+lucky_number = 7
+pi = 3.14
+one_is_a_prime_number = 1
+name = "John"
+my_favourite_films = ["Matrix", "Inception"]
+profile_info = {"name": "John", "age": 30}
+marks = {"Math": 10, "English": 12}
+collection_of_coins = [1, 2, 5, 10]
+
+# Список усіх змінних
+sorted_variables = [
+    lucky_number,
+    pi,
+    one_is_a_prime_number,
+    name,
+    my_favourite_films,
+    profile_info,
+    marks,
+    collection_of_coins
+]
