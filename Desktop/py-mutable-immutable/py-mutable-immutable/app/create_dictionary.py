528b6c281fa86617bae9d76c6a6a287e17e7bd75
Architector <katusha2105@gmail.com>
2026-03-22 18:46:53 +0200

    Fix newline at end of main.py

--- Desktop/py-mutable-immutable/py-mutable-immutable/app/create_dictionary.py
+++ Desktop/py-mutable-immutable/py-mutable-immutable/app/create_dictionary.py
@@ -1,16 +1,16 @@
 from typing import Any, Dict
 
+
 def create_dictionary(*args: Any) -> Dict[Any, int]:
     result: Dict[Any, int] = {}
     for index, arg in enumerate(args):
         if (
-    isinstance(arg, (int, float, str, bool, type(None), tuple))
-    or callable(arg)
-):
+            isinstance(arg, (int, float, str, bool, type(None), tuple))
+            or callable(arg)
+        ):
             result[arg] = index
         else:
             print(f"Cannot add {arg} to the dict!")
     return result
-
 
 
