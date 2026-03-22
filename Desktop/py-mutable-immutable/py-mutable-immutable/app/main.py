10c913676e23a327e09f3ee3b1db19a8565d916d
Architector <katusha2105@gmail.com>
2026-03-22 18:24:21 +0200

    Implement sorted_variables correctly

--- Desktop/py-mutable-immutable/py-mutable-immutable/app/main.py
+++ Desktop/py-mutable-immutable/py-mutable-immutable/app/main.py
@@ -1,25 +1,25 @@
-# app/main.py
-
-# Задаємо змінні для тестів
 lucky_number = 7
 pi = 3.14
-one_is_a_prime_number = 1
-name = "John"
+one_is_a_prime_number = False
+name = "Kate"
 my_favourite_films = ["Matrix", "Inception"]
-profile_info = {"name": "John", "age": 30}
-marks = {"Math": 10, "English": 12}
-collection_of_coins = [1, 2, 5, 10]
+profile_info = {"age": 20, "city": "Lviv"}
+marks = [5, 4, 3]
+collection_of_coins = {1, 2, 5}
 
-# Список усіх змінних
-sorted_variables = [
-    lucky_number,
-    pi,
-    one_is_a_prime_number,
-    name,
-    my_favourite_films,
-    profile_info,
-    marks,
-    collection_of_coins
+sorted_variables = {
+    "immutable": [
+        lucky_number,
+        pi,
+        one_is_a_prime_number,
+        name,
+    ],
+    "mutable": [
+        my_favourite_films,
+        profile_info,
+        marks,
+        collection_of_coins,
+    ],
+}
 
-]
 
