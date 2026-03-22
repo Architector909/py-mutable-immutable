5d3a41bde76abd1bcd3e9e261dc4f96753c0e187
Architector <katusha2105@gmail.com>
2026-03-22 19:25:01 +0200

    1

--- app/main.py
+++ app/main.py
@@ -1,17 +1,26 @@
-lucky_number = 777
-pi = 3.14
-one_is_a_prime_number = False
-name = "Richard"
-my_favourite_films = [
-    "The Shawshank Redemption",
-    "The Lord of the Rings: The Return of the King",
-    "Pulp Fiction",
-    "The Good, the Bad and the Ugly",
-    "The Matrix",
-]
-profile_info = ("michel", "michel@gmail.com", "12345678")
-marks = {
-    "John": 4,
-    "Sergio": 3,
+# Пример переменных
+lucky_number = 7            # int — immutable
+pi = 3.14                   # float — immutable
+one_is_a_prime_number = True # bool — immutable
+name = "Jessica"            # str — immutable
+
+my_favourite_films = ["Inception", "Pulp Fiction"]
+profile_info = {"age": 20, "city": "Lviv"}
+marks = [5, 4, 3]
+collection_of_coins = {1, 2, 5} 
+
+
+sorted_variables = {
+    "immutable": [
+        lucky_number,
+        pi,
+        one_is_a_prime_number,
+        name,
+    ],
+    "mutable": [
+        my_favourite_films,
+        profile_info,
+        marks,
+        collection_of_coins,
+    ],
 }
-collection_of_coins = {1, 2, 25}
