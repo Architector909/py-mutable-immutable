a09339839817423969c30ca1a48fe3939750f18c
Architector <katusha2105@gmail.com>
2026-03-22 18:16:24 +0200

    Remove submodule and add project files normally

---
+++ Desktop/py-mutable-immutable/py-mutable-immutable/app/main.py
@@ -0,0 +1,25 @@
+# app/main.py
+
+# Задаємо змінні для тестів
+lucky_number = 7
+pi = 3.14
+one_is_a_prime_number = 1
+name = "John"
+my_favourite_films = ["Matrix", "Inception"]
+profile_info = {"name": "John", "age": 30}
+marks = {"Math": 10, "English": 12}
+collection_of_coins = [1, 2, 5, 10]
+
+# Список усіх змінних
+sorted_variables = [
+    lucky_number,
+    pi,
+    one_is_a_prime_number,
+    name,
+    my_favourite_films,
+    profile_info,
+    marks,
+    collection_of_coins
+
+]
+
